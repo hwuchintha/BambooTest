551329cbf76c8fde8893e51fcad1b4ea9ece61c4
Uday Kiran Chintha <hw.uchintha@gmail.com>
2017-05-18 14:17:20 -0400

    Test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Bamboo test");
+            Console.ReadLine();
         }
     }
 }
